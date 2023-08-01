3735429efba96eaa95409da60f8fa42291f5f8a1
LevittC17 <LeewednesdayC17@protonmail.com>
2023-08-01 21:56:18 +0000

    an endpoint that retrieves the number of object by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,9 +8,18 @@ Return a JSON: "status": "OK"
 
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'])
 def get_status():
     """jsonify status response"""
     return jsonify({"status": "OK"})
+
+
+def db_status():
+    """Retrieve the number of each object type"""
+    obj_types = ['Amenity', 'City', 'Place', 'Review', 'State', 'User']
+    obj_count = {obj_type.lower(): storage.count(obj_type)
+                 for obj_t in obj_type}
+    return jsonify(obj_count)
